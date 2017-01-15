0c7a403d89730b30bf4e9fd131a91effe0e45d8b
Bony5690 <jerrybony@yahoo.com>
2017-01-15 10:13:54 -0600

    fade in and out effects

--- Js/script.js
+++ Js/script.js
@@ -1,21 +1,21 @@
-
-
-// $(document).ready(function(){
-//    fadeInOut();
-//
-// });
-
-
 $(function () {
     var $element = $('.welcome');
     var $element2 = $('.freelance');
+    var $element3 = $('.freelancer2');
 
     function fadeInOut () {
         $element.fadeIn(1000, function () {
             $element.fadeOut(1500, function () {
                 $element2.fadeIn(1500).removeClass('hidden');
-                $element2.fadeOut(1500);
-                setTimeout(fadeInOut, '4000');
+                $element2.fadeOut(1500, function(){
+                  $element3.fadeIn(1500).removeClass('hidden');
+                  $element3.fadeOut(1500, function(){
+                      setTimeout(fadeInOut, '1000');
+                  })
+
+                });
+
+
 
                 });
               });
@@ -24,54 +24,3 @@ $(function () {
 
     fadeInOut();
 });
-
-// $(function () {
-//     var $element = $('.welcome');
-//     var $element2 = $('.freelance');
-//     function fadeInOut () {
-//         $element.fadeIn(1000, function () {
-//             $element.fadeOut(1500, function () {
-//              $element2.fadeIn(1500).removeClass(function(){
-//                 $element2.fadeOut(1500, function(){
-//                    setTimeout(fadeInOut, 500);
-//                 });
-//               });
-//             });
-//         });
-//     }
-//
-//     fadeInOut();
-// });
-//
-//
-
-
-// function fade(){
-//
-//     $(".welcome").hide().fadeIn(3000);
-//       $(".welcome").fadeOut("fast", function(){
-//         $("freelance").hide().fadeIn(3000, function(){
-//           setTimeout(fade, 500);
-//         });
-//
-//
-//
-//       });
-//
-//       }
-
-
-
-//       function fade(){
-//
-//           $(".welcome").hide().fadeIn(3000);
-//           $(".welcome").fadeOut("fast", function(){
-//             $(".freelance").fadeIn(3000);
-//           // setTimeout(fade, 500);
-//
-//         });
-// }
-// function fadeWorks(){
-//   $("freelancer2").fadeIn(3000).removeClass("hidden");
-//
-// }
