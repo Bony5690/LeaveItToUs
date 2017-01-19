0cc3a54463baaf0e3d14cc2d103960c827e710fc
Bony5690 <jerrybony@yahoo.com>
2017-01-18 22:44:27 -0600

    scroll to top button

--- Js/script.js
+++ Js/script.js
@@ -1,3 +1,23 @@
+var amountScrolled = 300;
+
+$(window).scroll(function() {
+	if ( $(window).scrollTop() > amountScrolled ) {
+		$('a.back-to-top').fadeIn('slow');
+	} else {
+		$('a.back-to-top').fadeOut('slow');
+	}
+});
+
+
+$('a.back-to-top').click(function() {
+	$('html, body').animate({
+		scrollTop: 0
+	}, 700);
+	return false;
+});
+
+
+
 $(function () {
     var $element = $('.welcome');
     var $element2 = $('.freelance');
